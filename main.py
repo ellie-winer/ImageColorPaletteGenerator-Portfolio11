b1723aa263e4284113f6f859824e99d949bf7544
ellie-winer <elliewiner3@gmail.com>
2024-10-09 18:17:22 -0400

    adding dotenv to requirements.txt

--- main.py
+++ main.py
@@ -4,34 +4,34 @@ import numpy as np
 import webcolors
 from collections import Counter
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB upload limit
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-
-
 def get_color_name(rgb_color):
-    """
-    Get the color name and HEX code for a given RGB color.
-    """
+    ## webcolors doesn't know most colors but in an attempt to try another route i changed code from what's below this to what is in use
+    # try:
+    #     name = webcolors.rgb_to_name(rgb_color, spec='css3')
+    # except ValueError:
+    #     name = "Unknown Color"
+    hex_code = webcolors.rgb_to_hex(rgb_color)
     try:
-        name = webcolors.rgb_to_name(rgb_color, spec='css3')
+        name = webcolors.hex_to_name(hex_code, spec='css3')
     except ValueError:
         name = "Unknown Color"
-    hex_code = webcolors.rgb_to_hex(rgb_color)
     return name.title(), hex_code.upper()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-
         if 'image' not in request.files:
             return redirect(request.url)
         file = request.files['image']
@@ -41,16 +41,13 @@ def index():
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filepath)
 
-
             image = Image.open(filepath)
             image = image.convert('RGB')
             img_array = np.array(image)
             pixels = img_array.reshape(-1, 3)
 
-
             counts = Counter(map(tuple, pixels))
             top_colors = counts.most_common(10)
-
 
             color_info = []
             for color, count in top_colors:
